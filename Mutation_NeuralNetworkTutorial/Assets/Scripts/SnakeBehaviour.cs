16b71f2456c7421206a796b42338fdf63fbcb8db
Sander Goetzee <sanderg92@gmail.com>
2020-04-30 14:27:59 +0200

    Added tail movement

--- Mutation_NeuralNetworkTutorial/Assets/Scripts/SnakeBehaviour.cs
+++ Mutation_NeuralNetworkTutorial/Assets/Scripts/SnakeBehaviour.cs
@@ -6,68 +6,86 @@ using static Grid;
 namespace Completed
 {
 
-public class SnakeBehaviour : MovingObject
-{
+    public class SnakeBehaviour : MovingObject
+    {
 
-    public GameObject tailPrefab;
-    private GameInformation gameInformation;
-    private Vector2 direction = Vector2.up;
-    private float speed = 1f;
+        public GameObject tailPrefab;
+        private GameInformation gameInformation;
+        private Vector2 direction = Vector2.up;
+        private float speed = 1f;
 
-    private List<Transform> tail;
-    public Cell[,] board;
+        private List<Transform> tail = new List<Transform>();
+        public Cell[,] board;
         // Start is called before the first frame update
-    protected override void Start()
-    {
-        gameInformation = FindObjectOfType<GameInformation>();
-        tail = new List<Transform>();
-        board = gameInformation.board;
-        gameObject.transform.position = board[5, 5].position;
-        StartCoroutine("MoveSnake");
-        base.Start();
-    }
+        protected override void Start()
+        {
+            gameInformation = FindObjectOfType<GameInformation>();
+            tail = new List<Transform>();
+            board = gameInformation.board;
+            gameObject.transform.position = board[5, 5].position;
+            // Move the Snake every 300ms
+            InvokeRepeating("Move", 1.0f, 1.0f);
+            base.Start();
+        }
 
-    private void AddTaill()
-    {
-        GameObject tailPart = Instantiate(tailPrefab, tail[tail.Count - 1].position, Quaternion.identity, this.transform);
-        tail.Add(tailPart.transform);
-    }
+        private void AddTaill()
+        {
+            Debug.Log("Added tail");
+            Vector2 v = transform.position;
+            // Load Prefab into the world
+            GameObject g = (GameObject)Instantiate(tailPrefab, v, Quaternion.identity);
 
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-        if(Input.GetKeyDown(KeyCode.A))
-        {
-            direction = new Vector2(-1, 0);
-        }
-        if (Input.GetKeyDown(KeyCode.W))
-        {
-            direction = new Vector2(0, 1);
-        }
-        if (Input.GetKeyDown(KeyCode.D))
-        {
-            direction = new Vector2(1, 0);
-        }
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            direction = new Vector2(0, -1);
-        }
-    }
+            // Keep track of it in our tail list
+            tail.Insert(0, g.transform);
 
-    IEnumerator MoveSnake()
-    {
-        for (; ; )
+            // Reset the flag
+            //ate = false;
+        }
+
+        // Update is called once per frame
+        void FixedUpdate()
         {
-            transform.position += new Vector3(direction.x, direction.y, 0) * speed;
-            board[(int)transform.position.x, (int)transform.position.y].space = Cell.Space.snake;
-            for(int i = 0; i < tail.Count -1; i++)
+            if (Input.GetKeyDown(KeyCode.A))
             {
-                tail[i + 1].position = tail[i].position;
+                direction = -Vector2.right;
             }
-            yield return new WaitForSeconds(1f);
+            if (Input.GetKeyDown(KeyCode.W))
+            {
+                direction = Vector2.up;
+            }
+            if (Input.GetKeyDown(KeyCode.D))
+            {
+                direction = Vector2.right;
+            }
+            if (Input.GetKeyDown(KeyCode.S))
+            {
+                direction = -Vector2.up;
+            }
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                AddTaill();
+            }
         }
-        
-    }
+
+        void Move()
+        {
+            // Save current position (gap will be here)
+            Vector2 v = transform.position;
+
+            // Move head into new direction (now there is a gap)
+            transform.Translate(direction);
+
+            // Do we have a Tail?
+            if (tail.Count > 0)
+            {
+                // Move last Tail Element to where the Head was
+                tail[tail.Count -1].position = v;
+
+                // Add to front of list, remove from the back
+                tail.Insert(0, tail[tail.Count -1]);
+                tail.RemoveAt(tail.Count - 1);
+            }
+        }
 
         protected override void OnCantMove<T>(T component)
         {
